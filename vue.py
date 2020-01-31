198e65b53e53dea22eee0be40814901e38ccfbc5
camille <trainacamille@gmail.com>
2020-01-31 17:03:14 +0100

    debut fond

--- vue.py
+++ vue.py
@@ -1,12 +1,24 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
 class View(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="logiciel Toeic")
         self.set_border_width(10)
-        self.resize(1000,700)
+        self.resize(600,500)
+
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_data("""
+			box{
+                background-color: red;
+			}
+		""")
+
+        Gtk.StyleContext.add_provider_for_screen(
+            Gdk.Screen.get_default(),
+            css_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL , spacing=6)
         self.add(vbox)
@@ -18,10 +30,15 @@ class View(Gtk.Window):
 
         grid = Gtk.Grid()
         self.add(grid)
+        grid.set_column_homogeneous(True)
+        grid.set_row_spacing(30)
+
 
         grid2 = Gtk.Grid()
         self.add(grid2)
-
+        grid2.set_column_homogeneous(True)
+        grid2.set_row_homogeneous(True)
+        grid2.set_row_spacing(30)
 
         sel_tel = Gtk.FileChooserButton("Selectionner un dossier",Gtk.FileChooserAction.SELECT_FOLDER)
         sel_tel.set_action(Gtk.FileChooserAction.SELECT_FOLDER)
@@ -32,8 +49,8 @@ class View(Gtk.Window):
         entry1 = Gtk.Entry()
         entry1.set_text("Nom du TOEIC")
 
-        grid.add(entry1)
-        grid.add(button1)
+        grid.attach(entry1,0,0,5,1)
+        grid.attach(button1,0,3,5,1)
 
         stack.add_titled(grid, "enr", "Enregistrer correction")
 
@@ -42,19 +59,23 @@ class View(Gtk.Window):
         sel_cor.set_action(Gtk.FileChooserAction.SELECT_FOLDER)
 
         button2 = Gtk.Button(label="Lancer la correction")
+        button3 = Gtk.Button(label="Afficher resultats")
+        button3.set_sensitive(False)
 
         box = Gtk.ComboBox()
 
-        grid2.attach(box, 1, 0, 2, 1)
-        grid2.attach(sel_cor, 3,0, 1, 2)
-        grid2.attach(button2, 1, 2, 1, 1)
+        grid2.attach(box, 0, 0, 5, 1)
+        grid2.attach(sel_cor, 6,0, 5, 1)
+        grid2.attach(button2, 3, 3, 5, 1)
+        grid2.attach(button3, 3, 4, 5, 1)
 
         stack.add_titled(grid2, "cor", "Corriger")
 
         stack_switcher = Gtk.StackSwitcher()
         stack_switcher.set_stack(stack)
-        vbox.pack_start(stack_switcher, True, True, 0)
-        vbox.pack_start(stack, True, True, 0)
+        vbox.pack_start(stack_switcher, False , False, 0)
+        vbox.pack_start(stack, True, False, 0)
+
 
 
 win = View()
