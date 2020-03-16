4fadd5abdd68332aa401ebe4aee3b00a391fe618
edysabine <awoedysabine.tchalla@esprit.tn>
2020-03-16 19:28:02 +0100

    necessaire

--- aide.py
+++ aide.py
@@ -6,7 +6,7 @@ def order_points(pts):
 	# such that the first entry in the list is the top-left,
 	# the second entry is the top-right, the third is the
 	# bottom-right, and the fourth is the bottom-left
-	rect = np.zeros((4, 2), dtype = "int32")
+	rect = np.zeros((4, 2), dtype = "float32")
 	# the top-left point will have the smallest sum, whereas
 	# the bottom-right point will have the largest sum
 	s = pts.sum(axis = 1)
@@ -25,7 +25,6 @@ def four_point_transform(image, pts):
 	# obtain a consistent order of the points and unpack them
 	# individually
 	rect = order_points(pts)
-	print(rect)
 	(tl, tr, br, bl) = rect
 	# compute the width of the new image, which will be the
 	# maximum distance between bottom-right and bottom-left
@@ -54,3 +53,129 @@ def four_point_transform(image, pts):
 	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 	# return the warped image
 	return warped
+
+def grab_contours(cnts):
+    # if the length the contours tuple returned by cv2.findContours
+    # is '2' then we are using either OpenCV v2.4, v4-beta, or
+    # v4-official
+    if len(cnts) == 2:
+        cnts = cnts[0]
+
+    # if the length of the contours tuple is '3' then we are using
+    # either OpenCV v3, v4-pre, or v4-alpha
+    elif len(cnts) == 3:
+        cnts = cnts[1]
+
+    # otherwise OpenCV has changed their cv2.findContours return
+    # signature yet again and I have no idea WTH is going on
+    else:
+        raise Exception(("Contours tuple must have length 2 or 3, "
+            "otherwise OpenCV changed their cv2.findContours return "
+            "signature yet again. Refer to OpenCV's documentation "
+            "in that case"))
+
+    # return the actual contours array
+    return cnts
+
+
+def sort_contours(cnts, method="left-to-right"):
+	# initialize the reverse flag and sort index
+	reverse = False
+	i = 0
+	# handle if we need to sort in reverse
+	if method == "right-to-left" or method == "bottom-to-top":
+		reverse = True
+	# handle if we are sorting against the y-coordinate rather than
+	# the x-coordinate of the bounding box
+	if method == "top-to-bottom" or method == "bottom-to-top":
+		i = 1
+	# construct the list of bounding boxes and sort them from top to
+	# bottom
+	boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+	(cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+		key=lambda b:b[1][i], reverse=reverse))
+	# return the list of sorted contours and bounding boxes
+	return (cnts, boundingBoxes)
+
+
+def rotationimage(pts, ecart):
+    compteurx = [0,0,0]
+    compteury = [0,0,0]
+    valeurx = [-1,-1,-1]
+    valeury = [-1,-1,-1]
+    posminx = -1
+    posminy = -1
+
+    for i in range(0,5):
+        if posminx == -1:
+            posminx = pts[i][0]
+        if valeurx[0] == -1:
+            valeurx[0] = pts[i][0]
+        if pts[i][0] < valeurx[0]+ecart and pts[i][0] > valeurx[0]-ecart:
+            compteurx[0] += 1
+            if posminx > pts[i][0]:
+                posminx = 0
+        else:
+            if valeurx[1] == -1:
+                valeurx[1] = pts[i][0]
+            if pts[i][0] < valeurx[1]+ecart and pts[i][0] > valeurx[1]-ecart:
+                compteurx[1] += 1
+                if posminx > pts[i][0]:
+                    posminx = 1
+            else:
+                if valeurx[2] == -1:
+                    valeurx[2] = pts[i][0]
+                compteurx[2] += 1
+                if posminx > pts[i][0]:
+                    posminx = 2
+    
+    for i in range(0,5):
+        if posminy == -1:
+            posminy = pts[i][1]
+        if valeury[0] == -1:
+            valeury[0] = pts[i][1]
+        if pts[i][1] < valeury[0] + ecart and pts[i][1] > valeury[0]-ecart:
+            compteury[0] += 1
+            if posminy > pts[i][1]:
+                posminy = 0
+        else:
+            if valeury[1] == -1:
+                valeury[1] = pts[i][1]
+            if pts[i][1] < valeury[1]+ecart and pts[i][1] > valeury[1]-ecart:
+                compteury[1] += 1
+                if posminy > pts[i][1]:
+                    posminy = 1
+            else:
+                if valeury[2] == -1:
+                    valeury[2] = pts[i][1]
+                compteury[2] += 1
+                if posminy > pts[i][1]:
+                    posminy = 2
+
+    print (pts)
+    print (compteurx)
+    print (compteury)
+    print (valeurx)
+    print (valeury)
+    print (posminx)
+    print (posminy)
+
+    if compteurx[2]==0:
+        if compteurx[posminx]==3:
+            return 90
+        else:
+            return -90
+    else:
+        if compteury[posminy]==3:
+            return 180
+        else:
+            return 0
+
+
+'''
+*   *   *
+
+*       *
+'''
+pts = [[100,100], [50,0], [100,0], [0,0], [0,100]]
+pts= [(1598.0, 48.0), (99.0, 41.0), (1593.0, 1153.0), (89.0, 2267.0), (1595.0, 2272.0)]
